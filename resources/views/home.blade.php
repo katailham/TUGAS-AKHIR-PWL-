4f97c2a7c1815270bad637388446de0e217cc704
katailham <ilhamasep028@gmail.com>
2021-04-29 18:02:29 +0700

    crud barang,merek,kategori,user

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+@extends('adminlte::page')
+@section('title', 'Welcome')
+@section('content')
+ <div class="row">
+ <div class="col-6">
+ <div class="card">
+ <div class="card card-widget widget-user">
+ <div class="widget-user-header text-white" style="background: url('gambar/bg.png') center center; background-size:cover;">
+ <h3 class="widget-user-username text-center">Sopingi</h3>
+ <h5 class="widget-user-desc text-center">Universitas Duta Bangsa Surakarta</h5>
+ </div> 
+1
+ <div class="widget-user-image">
+ <img class="img-circle" src="gambar/depaan.png">
+ </div>
+ <div class="card-footer">
+ <center><img src="gambar/depan1.png" width="90%"></center>
+ </div>
+ </div>
+ </div>
+ </div>
+ <div class="col-6">
+ <div class="card">
+ <div class="card card-widget widget-user">
+ <div class="widget-user-header text-white" style="background: url('gambar/bg.png') center center; background-size:cover;">
+ <h3 class="widget-user-username text-center">Sri Sumarlinda</h3>
+ <h5 class="widget-user-desc text-center">Universitas Duta Bangsa Surakarta</h5>
+ </div>
+ <div class="widget-user-image">
+ <img class="img-circle" src="gambar/depan3.png">
+ </div>
+ <div class="card-footer">
+ <center><img src="gambar/depan5.png" width="90%"></center>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+@stop 
